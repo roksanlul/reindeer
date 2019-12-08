86ddc4330eb7df218fcc095261ebb7fa8fa07898
Ilya Karataev <ilkarvet89@gmail.com>
2019-12-08 17:02:01 +0500

    add new files

---
+++ js/firstGame.js
@@ -0,0 +1,11 @@
+// Arcade Shooter game
+
+// Get a reference to the canvas DOM element
+var canvas = document.getElementById('canvas');
+// Get the canvas drawing context
+var context = canvas.getContext('2d');
+var img = document.getElementById("photo");
+
+// Start the game
+menu();
+canvas.focus();
